5c86a002bdf70a8f97dfff7d812fef1698ba3192
Krishnapriya12345678 <72981926+Krishnapriya12345678@users.noreply.github.com>
2020-11-17 19:30:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var waste,wasteBody,waste_options
+var ground,groundBody,ground_options, box1,box2,box3,waste,waste
+  
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 700);
+  
+
+
+  ground_options={
+    isStatic:true
+  }
+  
+  
+	engine = Engine.create();
+  world = engine.world;
+  
+
+    
+    ground=createSprite(400,590,1200,10,ground_options)
+     World.add(world,ground)
+
+  groundBody=Bodies.rectangle(400,600,1200,10,ground_options)
+  World.add(world,groundBody)
+ 
+box1=new Box(580,530)
+box2=new Box(420,530)
+box3=new Box2(500,580,120,20)
+
+
+waste=new Waste(100,590,20);
+Engine.run(engine);
+    
+}
+
+
+function draw() {
+ 
+ background('black');
+fill('red')
+text('PUT THE WASTE IN THE RIGHT PLACE',300,100)
+fill('green')
+text('DUSTBIN',490,450)
+ drawSprites();
+
+
+ground.x=groundBody.position.x
+ground.y=groundBody.position.y
+
+box1.display();
+box2.display();
+box3.display();
+waste.display();
+
+
+}
+
+
+
+
+
+
+function keyPressed(){
+  if(keyCode===UP_ARROW){
+   Matter.Body.applyForce(waste.body,waste.body.position,{x:40,y:-50})
+  }
+}
+
+
+
+
+
+
+
---
+++ waste.js
@@ -0,0 +1,13 @@
+class Waste extends BaseClass{
+    constructor(x,y){
+        super(x,y,20)
+        this.image=loadImage('Waste.png')
+        
+    }
+
+display(){
+    super.display();
+}
+
+
+}
